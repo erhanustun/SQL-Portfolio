954d436fe550301e1035b69602f67c0340aa0101
Erhan Üstün <erhan.ustun.marun@hotmail.com>
2025-08-13 11:54:16 +0300

    Update 02_Basic_Queries.sql

--- 02_E-commerce_Relational_Sql_Analysis/02_Basic_Queries.sql
+++ 02_E-commerce_Relational_Sql_Analysis/02_Basic_Queries.sql
@@ -1,7 +1,18 @@
-### Total Orders and Revenue Analysis
+-- 1. Total Orders and Total Revenue
 SELECT 
     COUNT(DISTINCT o.order_id) AS total_orders,
     ROUND(SUM(oi.price + oi.freight_value), 2) AS total_revenue
 FROM orders o
 JOIN order_items oi 
     ON o.order_id = oi.order_id;
+
+-- 2. Top Customers by Number of Orders
+SELECT 
+    c.customer_unique_id,
+    COUNT(DISTINCT o.order_id) AS total_orders
+FROM customers c
+JOIN orders o 
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_unique_id
+ORDER BY total_orders DESC
+LIMIT 10;
